a28fe14f1033333280668c28f3b07ae826fd8609
felpsnaitor <felpsnaitor@gmail.com>
2024-05-27 16:27:59 -0300

    Feito as devidas alterações

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia("1234", "A1b44", "321514A412", 4);
+nokia.Ligar();
+nokia.InstalarAplicativo("ZapZap");
+
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone("1234", "A1b44", "321514A412", 4);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+
+
